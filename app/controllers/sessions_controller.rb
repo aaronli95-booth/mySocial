6c474d7a3c0c5af9f61dc022163cd808fc4e820b
Aaron Li Davis <A@Aarons-MacBook-Air.local>
2025-04-16 22:10:56 -0500

    0416 2210 fixed some minor bugs with log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to root_path
+    flash.clear
+    redirect_to root_path, notice: "Signed out successfully"
   end
 end
---
+++ app/models/friendship.rb
@@ -0,0 +1,12 @@
+# db/migrate/XXXX_create_friendships.rb
+class CreateFriendships < ActiveRecord::Migration[7.1]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: { to_table: :users }
+      t.boolean :confirmed, default: false
+      t.timestamps
+    end
+    add_index :friendships, [:user_id, :friend_id], unique: true
+  end
+end
